b37143725f26000215c080af7e79586803902e93
Denis Kartsev <62169239+tdlcompiler@users.noreply.github.com>
2023-05-19 01:03:27 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Моя веб-страница</title>
+</head>
+<body>
+    <h1>Добро пожаловать на мою страницу</h1>
+    <?php
+        echo "Текущее время: " . date("Y-m-d H:i:s");
+    ?>
+</body>
+</html>
